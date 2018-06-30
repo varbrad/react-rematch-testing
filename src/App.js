4fe69ffb8ccf5706a6445efd6c828985ca6cf5b0
Brad Davies <varbrad@users.noreply.github.com>
2018-06-30 13:18:51 +0100

    Improve linting

--- src/App.js
+++ src/App.js
@@ -4,21 +4,13 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import HomeScreen from './screens/home/HomeScreen'
 
 class App extends React.PureComponent {
-  render () {
+  render() {
     return (
       <Router>
         <div>
-          <h1>
-Hello!
-          </h1>
-          <Link to="/">
-            {'Home'}
-          </Link>
-          <Route
-            exact
-              path="/"
-              component={HomeScreen}
-          />
+          <h1>Hello!</h1>
+          <Link to="/">{'Home'}</Link>
+          <Route exact path="/" component={HomeScreen} />
         </div>
       </Router>
     )
--- src/screens/home/HomeScreen.js
+++ src/screens/home/HomeScreen.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React from 'react'
 
 class HomeScreen extends React.PureComponent {
   render() {
