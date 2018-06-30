9abdbeaecc5fd406957278d88d74cad89a399a91
Brad Davies <varbrad@users.noreply.github.com>
2018-06-30 13:15:42 +0100

    Initial setup with linting & routing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
-class App extends Component {
-  render() {
+import HomeScreen from './screens/home/HomeScreen'
+
+class App extends React.PureComponent {
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <Router>
+        <div>
+          <h1>
+Hello!
+          </h1>
+          <Link to="/">
+            {'Home'}
+          </Link>
+          <Route
+            exact
+              path="/"
+              component={HomeScreen}
+          />
+        </div>
+      </Router>
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/screens/home/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+class HomeScreen extends React.PureComponent {
+  render() {
+    return (
+      <div>
+        <p>Hello!</p>
+      </div>
+    )
+  }
+}
+
+export default HomeScreen
