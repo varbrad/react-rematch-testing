50c9e799b6c095b95a7be7003abe8e57c1decfc8
Brad Davies <varbrad@users.noreply.github.com>
2018-06-30 14:18:27 +0100

    Integrate react-redux and rematch

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+import { Provider } from 'react-redux'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
+import store from './store'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const Root = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
+
+ReactDOM.render(<Root />, document.getElementById('root'))
 registerServiceWorker()
---
+++ src/store/index.js
@@ -0,0 +1,5 @@
+import { init } from '@rematch/core'
+
+export default init({
+  models: {}
+})
