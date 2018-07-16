b2b0577c3a6dd216e1634cafa334f9be8e59186f
Brad Davies <varbrad@users.noreply.github.com>
2018-07-16 21:52:02 +0100

    rematch persist

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,15 @@
 import { init } from '@rematch/core'
+import persist from '@rematch/persist'
 
 import todos from './todos/'
+
+const persistPlugin = persist({
+  throttle: 1000
+})
 
 export default init({
   models: {
     todos
-  }
+  },
+  plugins: [persistPlugin]
 })
